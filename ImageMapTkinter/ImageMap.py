1cb9f62d26f96c2a9833988c492db2f9622521ba
Argay Bence <76647095+argaybence@users.noreply.github.com>
2022-06-11 09:58:57 +0200

    More flexible triangle mapping
    With this update It works with every single triangle.

--- ImageMapTkinter/ImageMap.py
+++ ImageMapTkinter/ImageMap.py
@@ -17,6 +17,16 @@ class ImageMapping:
         # the geometry, so now the middle of the frame has the coordinate (0;0)
         self.x, self.y = coordinate_translater(self.geometry, (self.x, self.y))
 
+    @staticmethod
+    def between(x, x1, x2):
+        if x1 < x2:  # -20 < -150
+            if x1 <= x <= x2:
+                return True
+
+        if x1 >= x >= x2:
+            return True
+        return False
+
     def triangle(self):
         # Combine creates all variation of coordinate pairs (without duplications)
         # Proper_lines goes through these coordinate pairs, and select the correct ones (the lines on each side - which
@@ -24,19 +34,44 @@ class ImageMapping:
         # Sorting it to avoid wrong calculation (avoid the exchange of the right_x, and left_x)
         lines = sorted(proper_lines(combine(self.coords, duplicate=False), len(self.coords)), key=lambda x: x[0])
 
-        # The lowest and the highest y coordinate on the line
-        min_y, max_y = value_set(lines)
+        if len(lines) == 2:
+            x1 = {two_point_equation(lines[0][0], lines[0][1], self.y): [lines[0][0][1], lines[0][1][1]]}
+            x2 = {two_point_equation(lines[1][0], lines[1][1], self.y): [lines[1][0][1], lines[1][1][1]]}
 
-        left_x, right_x = None, None
+            y_values = list(x1.values())[0]
 
-        if min_y <= self.y <= max_y:
-            left_x = two_point_equation(lines[0][0], lines[0][1], self.y)
-            right_x = two_point_equation(lines[1][0], lines[1][1], self.y)
+            if y_values[0] <= self.y <= y_values[1]:
+                left_x, right_x = list(x1.keys())[0], list(x2.keys())[0]
+                if left_x <= self.x <= right_x:
+                    self.command()
+        else:
+            x1 = {two_point_equation(lines[0][0], lines[0][1], self.y): [lines[0][0][1], lines[0][1][1]]}
+            x2 = {two_point_equation(lines[1][0], lines[1][1], self.y): [lines[1][0][1], lines[1][1][1]]}
+            x3 = {two_point_equation(lines[2][0], lines[2][1], self.y): [lines[2][0][1], lines[2][1][1]]}
 
-        if left_x is not None:
+            x_values = [x1, x2, x3]
 
-            if left_x <= self.x <= right_x:
-                self.command()
+            distances = [list(x1.values())[0], list(x2.values())[0], list(x3.values())[0]]
+            largest = 0
+            index = 0
+
+            for i in range(3):
+                if abs(distances[i][0] - distances[i][1]) > largest:
+                    largest = abs(distances[i][0] - distances[i][1])
+                    index = i
+
+            for j in range(3):
+                if j == index:
+                    continue
+                y_values = x_values[j][list(x_values[j].keys())[0]]
+
+                if self.between(self.y, y_values[0], y_values[1]):
+                    left_x = list(x_values[index].keys())[0]
+                    right_x = list(x_values[j].keys())[0]
+
+                    if self.between(self.x, left_x, right_x):
+
+                        self.command()
 
     def rectangle(self):
         # Combine creates all variation of coordinate pairs (without duplications)
